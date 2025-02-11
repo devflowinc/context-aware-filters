c5e1e72a793a1d56b9d26516a105ad0b0ff91fb5
cdxker <fdenzell@gmail.com>
2025-02-10 17:42:34 -0800

    feature: working with basic tests

--- src/main.rs
+++ src/main.rs
@@ -1,97 +1,193 @@
-use openai_dive::v1::resources::chat::{ChatMessage, ChatMessageContent};
+use openai_dive::v1::{
+    api::Client,
+    resources::chat::{
+        ChatCompletionParametersBuilder, ChatCompletionResponseFormat, ChatMessage,
+        ChatMessageContent, JsonSchemaBuilder,
+    },
+};
+use reqwest::ClientBuilder;
+use serde::{Deserialize, Serialize};
 
-pub async fn make_search() {
+use tests::*;
+
+mod tests;
+
+#[derive(Deserialize, Serialize)]
+pub struct FilterOutput {
+    selected_tags: Vec<String>,
 }
 
-pub async fn make_filter() {
+pub async fn make_search() {}
+
+pub async fn make_filter(available_tags: Vec<String>, convo: &ExampleConvo) -> FilterOutput {
+    let mut messages = convo.previous_messages.clone();
+    messages.push(
+        ChatMessage::User {
+            content: ChatMessageContent::Text(format!("Based on the above conversastion, which of the following tags should I filter by? {:?}?", available_tags)),
+            name: Some("search_expert".to_string())
+        }
+    );
+
+    let parameters = ChatCompletionParametersBuilder::default()
+        .model("gpt-4o-mini")
+        .messages(messages)
+        .response_format(ChatCompletionResponseFormat::JsonSchema(
+            JsonSchemaBuilder::default()
+                .name("math_reasoning")
+                .schema(serde_json::json!({
+                    "type": "object",
+                    "properties": {
+                        "selected_tags": { "type": "array", "items": { "type": "string"} }
+                    },
+                    "required": ["selected_tags"],
+                    "additionalProperties": false
+                }))
+                .strict(true)
+                .build()
+                .expect("failed to build schema"),
+        ))
+        .build()
+        .expect("failed to build parameters");
+
+    let client = Client {
+        headers: None,
+        project: None,
+        api_key: std::env::var("OPENAI_API_KEY").expect("Please add an api key"),
+        http_client: reqwest::Client::new(),
+        base_url: "https://api.openai.com/v1".to_string(),
+        organization: None,
+    };
+
+    let result = client
+        .chat()
+        .create(parameters)
+        .await
+        .expect("failed to run chat");
+
+    match result.choices.first().expect("HI").message.clone() {
+        ChatMessage::Assistant {
+            content: Some(ChatMessageContent::Text(content)),
+            ..
+        } => serde_json::from_str(&content).expect(""),
+        _ => FilterOutput {
+            selected_tags: vec![],
+        },
+    }
 }
 
 pub async fn create_message(conversastion: ExampleConvo) {
-    println!("Running conversastion for {}", conversastion.name);
+    println!(
+        "------------------------- Running conversastion for {} -------------------------------",
+        conversastion.name
+    );
+    let mut tag_filters: Vec<String> = vec![];
+    if let Some(tags) = conversastion.available_tags.clone() {
+        let results = make_filter(tags.clone(), &conversastion).await;
+        println!("Applied filters {:?}", results.selected_tags);
+        tag_filters = results.selected_tags;
+    }
 
-    let last_message = ChatMessageContent::Text(format!(
-        "Here's my prompt: {} \n\n {} {}",
-        user_message.clone(),
-        rag_prompt, // prompt
-        rag_content, // chunks
-    ));
+    println!("OUTPUT: \n\n");
+
+    let latest_user_message = match conversastion
+        .previous_messages
+        .clone()
+        .last()
+        .expect("it is here")
+    {
+        ChatMessage::User {
+            content: ChatMessageContent::Text(content),
+            ..
+        } => content.clone(),
+        _ => unreachable!(),
+    };
+
+    // Search with Trieve
+    let search_req_payload = serde_json::json!({
+        "query": latest_user_message,
+        "search_type": "hybrid",
+        "filters": {
+            "must": [
+                {
+                    "field": "tag_set",
+                    "match_any": tag_filters
+                }
+            ]
+        }
+    });
+
+    let reqwest_client = ClientBuilder::new()
+        .build()
+        .expect("Failed to create reqwest client");
+
+    let mut header_map: reqwest::header::HeaderMap = reqwest::header::HeaderMap::new();
+    header_map.insert(
+        "Content-Type",
+        reqwest::header::HeaderValue::from_static("application/json"),
+    );
+    header_map.insert(
+        "Authorization",
+        reqwest::header::HeaderValue::from_str(
+            std::env::var("TRIEVE_API_KEY")
+                .expect("TRIEVE_API_KEY is not set")
+                .as_str(),
+        )
+        .unwrap(),
+    );
+    header_map.insert(
+        "TR-Dataset",
+        reqwest::header::HeaderValue::from_str(
+            std::env::var("TRIEVE_DATASET_ID")
+                .expect("TRIEVE_DATASET_ID is not set")
+                .as_str(),
+        )
+        .unwrap(),
+    );
+    header_map.insert(
+        "X-API-Version",
+        reqwest::header::HeaderValue::from_static("V2"),
+    );
+
+    let search_req_resp = reqwest_client
+        .post("https://api.trieve.ai/api/chunk/search")
+        .headers(header_map)
+        .body(serde_json::to_string(&search_req_payload).unwrap())
+        .send()
+        .await
+        .expect("Failed to search")
+        .text()
+        .await
+        .expect("Failed to get body");
+
+    println!("search resp {:?}", search_req_resp);
+
+    // let last_message = ChatMessageContent::Text(format!(
+    //     "Here's my prompt: {} \n\n {} {}",
+    //     user_message.clone(),
+    //     rag_prompt,  // prompt
+    //     rag_content, // chunks
+    // ));
 }
 
-#[derive(Default)]
+#[derive(Default, Clone)]
 pub struct ExampleConvo {
     pub name: String,
-    pub messages: Vec<ChatMessage>,
-    pub tags: Option<String>,
+    pub previous_messages: Vec<ChatMessage>,
+    pub available_tags: Option<Vec<String>>,
 }
 
 #[tokio::main]
 pub async fn main() {
-    let examples = vec![make_a_search(), filtered_search(), just_respond()];
+    dotenvy::dotenv().expect("failed to load env vars");
+    let examples = vec![
+        make_a_search(),
+        filtered_search(),
+        just_respond(),
+        image_search(),
+        recomend_flooring()
+    ];
 
     for example in examples {
         create_message(example).await;
-    }
-}
-
-fn make_a_search() -> ExampleConvo {
-    ExampleConvo {
-        name: "Make a search".to_string(),
-        messages: vec![
-            ChatMessage::System {
-                content: ChatMessageContent::Text(
-                    " You are a chat bot designed to make my life easier.".to_string(),
-                ),
-                name: None,
-            },
-            ChatMessage::User {
-                content: ChatMessageContent::Text(
-                    " You are a chat bot designed to make my life easier. ".to_string(),
-                ),
-                name: None,
-            },
-        ],
-        ..Default::default()
-    }
-}
-
-fn filtered_search() -> ExampleConvo {
-    ExampleConvo {
-        name: "filtered_search".to_string(),
-        messages: vec![
-            ChatMessage::System {
-                content: ChatMessageContent::Text(
-                    " You are a chat bot designed to make my life easier.".to_string(),
-                ),
-                name: None,
-            },
-            ChatMessage::User {
-                content: ChatMessageContent::Text(
-                    " You are a chat bot designed to make my life easier. ".to_string(),
-                ),
-                name: None,
-            },
-        ],
-        ..Default::default()
-    }
-}
-
-fn just_respond() -> ExampleConvo {
-    ExampleConvo {
-        name: "Just respond".to_string(),
-        messages: vec![
-            ChatMessage::System {
-                content: ChatMessageContent::Text(
-                    " You are a chat bot designed to make my life easier.".to_string(),
-                ),
-                name: None,
-            },
-            ChatMessage::User {
-                content: ChatMessageContent::Text(
-                    " You are a chat bot designed to make my life easier. ".to_string(),
-                ),
-                name: None,
-            },
-        ],
-        ..Default::default()
     }
 }
---
+++ src/tests.rs
@@ -0,0 +1,153 @@
+use openai_dive::v1::resources::chat::{
+    ChatMessage, ChatMessageContent, ChatMessageContentPart, ChatMessageImageContentPart,
+    ImageUrlType,
+};
+
+use crate::ExampleConvo;
+
+pub fn make_a_search() -> ExampleConvo {
+    ExampleConvo {
+        name: "Make a search".to_string(),
+        available_tags: Some(vec![
+            "whiskey".to_string(),
+            "beer".to_string(),
+            "gin".to_string(),
+        ]),
+        previous_messages: vec![
+            ChatMessage::System {
+                content: ChatMessageContent::Text(
+                    " You are a chat bot designed to make my life easier.".to_string(),
+                ),
+                name: None,
+            },
+            ChatMessage::User {
+                content: ChatMessageContent::Text("What are some good Whiskey's for beginners".to_string()),
+                name: None,
+            },
+        ],
+        ..Default::default()
+    }
+}
+
+pub fn filtered_search() -> ExampleConvo {
+    ExampleConvo {
+        name: "filtered_search".to_string(),
+        available_tags: Some(vec![
+            "whiskey".to_string(),
+            "beer".to_string(),
+            "gin".to_string(),
+        ]),
+        previous_messages: vec![
+            ChatMessage::System {
+                content: ChatMessageContent::Text(
+                    " You are a chat bot designed to make my life easier.".to_string(),
+                ),
+                name: None,
+            },
+            ChatMessage::User {
+                content: ChatMessageContent::Text("Whiskey for beginners".to_string()),
+                name: None,
+            },
+        ],
+        ..Default::default()
+    }
+}
+
+pub fn just_respond() -> ExampleConvo {
+    ExampleConvo {
+        name: "Just respond".to_string(),
+        available_tags: Some(vec![
+            "whiskey".to_string(),
+            "beer".to_string(),
+            "gin".to_string(),
+        ]),
+        previous_messages: vec![
+            ChatMessage::System {
+                content: ChatMessageContent::Text(
+                    " You are a chat bot designed to make my life easier.".to_string(),
+                ),
+                name: None,
+            },
+            ChatMessage::User {
+                content: ChatMessageContent::Text("Whiskey for beginners".to_string()),
+                name: None,
+            },
+        ],
+        ..Default::default()
+    }
+}
+
+pub fn image_search() -> ExampleConvo {
+    ExampleConvo {
+        name: "Pick Hardwood Flooring".to_string(),
+        available_tags: Some(vec![
+            "Laminate Flooring".to_string(),
+            "Vinyl Flooring".to_string(),
+            "Carpet Tiles".to_string(),
+            "Unfinished Hardwood Flooring".to_string(),
+        ]),
+        previous_messages: vec![
+            ChatMessage::System {
+                content: ChatMessageContent::Text(
+                    "Always respond no matter what. You are a sales assistant for DesignShop. Your goal is to help self DesignShop's products. Be professional, brief, and to the point. Always use more words than just 'here' for your links. Answer questions to the best of your ability and avoid listing. Try to stick to only highlighting 1 or 2 products.".to_string()
+                ),
+                name: None,
+            },
+            ChatMessage::User {
+                content: ChatMessageContent::ContentPart(vec![
+                    ChatMessageContentPart::Image(ChatMessageImageContentPart {
+                        r#type: "image_url".to_string(),
+                        image_url: ImageUrlType {
+                            url: "https://images.ctfassets.net/zkiqcy76d1dl/3PEVFibbe2TWLQyttg2Spu/0527c9af7d3a0398a87c1e7effaf58a3/new-image-3e2b1cf7-dc34-44a3-8ca8-039031a329e3?q=80&w=2800&fm=webp".to_string(),
+                            detail: None,
+                        },
+                    }),
+                ]),
+                name: None,
+            },
+            ChatMessage::User {
+                content: ChatMessageContent::Text("Help me find a paint similar to this color".to_string()),
+                name: None
+            }
+        ],
+        ..Default::default()
+    }
+}
+
+pub fn recomend_flooring() -> ExampleConvo {
+    ExampleConvo {
+        name: "Pick Hardwood Flooring".to_string(),
+        available_tags: Some(vec![
+            "Laminate Flooring".to_string(),
+            "Vinyl Flooring".to_string(),
+            "Carpet Tiles".to_string(),
+            "Unfinished Hardwood Flooring".to_string(),
+            "Carpet".to_string(),
+        ]),
+        previous_messages: vec![
+            ChatMessage::System {
+                content: ChatMessageContent::Text(
+                    "Always respond no matter what. You are a sales assistant for DesignShop. Your goal is to help self DesignShop's products. Be professional, brief, and to the point. Always use more words than just 'here' for your links. Answer questions to the best of your ability and avoid listing. Try to stick to only highlighting 1 or 2 products.".to_string()
+                ),
+                name: None,
+            },
+            ChatMessage::User {
+                content: ChatMessageContent::ContentPart(vec![
+                    ChatMessageContentPart::Image(ChatMessageImageContentPart {
+                        r#type: "image_url".to_string(),
+                        image_url: ImageUrlType {
+                            url: "https://dma-render-stage.global.ssl.fastly.net/scenes/117/1727084965709_day.png?quality=80&width=816&format=webp".to_string(),
+                            detail: None,
+                        },
+                    }),
+                ]),
+                name: None,
+            },
+            ChatMessage::User {
+                content: ChatMessageContent::Text("I like how this flooring looks, please show me similar floors".to_string()),
+                name: None
+            }
+        ],
+        ..Default::default()
+    }
+}
